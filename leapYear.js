b9cc4f42d49b37b4277fd86a883cb396f7cde76a
ronakmojidra <mojidraronak@gmail.com>
2023-07-31 19:10:17 +0530

    Code Updated
    Code Updated

--- leapYear.js
+++ leapYear.js
@@ -1,11 +1,18 @@
-var checkLeapYear = function(year){
-    if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
-        console.log(`${year} is a leap year`);
-    } else {
-        console.log(`${year} is not a leap year`);
-    }
-}
+var checkLeapYear = function (year) {
+  if (year <= 0 || isNaN(year)) {
+    console.log(`${year}: Invalid Year`);
+  } else if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+    console.log(`${year} is a leap year`);
+  } else {
+    console.log(`${year} is not a leap year`);
+  }
+};
 checkLeapYear(2100);
 checkLeapYear(2024);
 checkLeapYear(2021);
 checkLeapYear(2040);
+checkLeapYear(-2040);
+checkLeapYear("Two Thousand twenty four");
+checkLeapYear(null);
+checkLeapYear(undefined);
+checkLeapYear(4);
--- triangle.js
+++ triangle.js
@@ -1,12 +1,27 @@
-var triangleType = function (side1,side2,side3) {
-    if (side1 === side2 && side1 === side3) {
-        console.log(`${side1} ${side2} ${side3}: Equilateral triangle`);
-    } else if (side1 === side2 || side1 === side3 || side2 === side3) {
-        console.log(`${side1} ${side2} ${side3}: Isosceles triangle`);
-    } else {
-        console.log(`${side1} ${side2} ${side3}: Scalene triangle`);
-    }
-}
-triangleType(45,45,45);
-triangleType(45,90,45);
-triangleType(30,45,90);
+var triangleType = function (side1, side2, side3) {
+  if (
+    side1 <= 0 ||
+    side2 <= 0 ||
+    side3 <= 0 ||
+    isNaN(side1, side2, side3) ||
+    side1 == "" ||
+    side2 == "" ||
+    side3 == ""
+  ) {
+    console.log(`${side1} ${side2} ${side3}: Invalid Data`);
+  } else if (side1 === side2 && side1 === side3) {
+    console.log(`${side1} ${side2} ${side3}: Equilateral triangle`);
+  } else if (side1 === side2 || side1 === side3 || side2 === side3) {
+    console.log(`${side1} ${side2} ${side3}: Isosceles triangle`);
+  } else {
+    console.log(`${side1} ${side2} ${side3}: Scalene triangle`);
+  }
+};
+triangleType(45, 45, 45);
+triangleType(45, 90, 45);
+triangleType(30, 45, 90);
+triangleType(-45, -25, -56);
+triangleType("twenty", "sixty", "ninety");
+triangleType(null, null, null);
+triangleType(undefined, undefined, undefined);
+triangleType(null, 45, 92);
